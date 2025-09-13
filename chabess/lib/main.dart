1815e07f0bf49520f25a193a7bc44e9a4d6f72cd
Luis Mario <43504168+LuisM711@users.noreply.github.com>
2025-09-13 13:42:11 -0700

    practica 3

--- chabess/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:chabess/src/app.dart';
-import 'package:flutter/cupertino.dart';
-
-void main() {
-  runApp(MyApp());
-}
---
+++ practica_03/lib/src/home_page.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:practica_03/src/datos_recibidos.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+
+  final ctrlnom = TextEditingController();
+  final data = Data(nombre: '', sexo: '');
+  int _valor = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepOrange[200],
+        title: const Text('Practica 03'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: ctrlnom,
+              keyboardType: TextInputType.name,
+              decoration: const InputDecoration(
+                hintText: 'Ingresa el nombre',
+                contentPadding: EdgeInsets.all(20)
+              ),
+            ),
+            const SizedBox(height: 20),
+            DropdownButton(
+              value: _valor,
+              items: const [
+                DropdownMenuItem(
+                  value: 1,
+                  child: Text('Mujer'),
+                ),
+                DropdownMenuItem(
+                  value: 2,
+                  child: Text('Hombre'),
+                )
+              ],
+              onChanged: (selected){
+                setState((){
+                  _valor = selected!;
+                });
+              },
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.orange[200],
+                foregroundColor: Colors.white,
+              ),
+              onPressed: () {
+                setState(() {
+                  data.nombre = ctrlnom.text;
+                  if (_valor ==1){
+                    data.sexo = 'Mujer';
+                  } else {
+                    data.sexo = 'Hombre';
+                  }
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => DatosPage(datos: data),
+                  ));
+                });
+              },
+              child: const Text('Enviar'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+class Data {
+  String nombre;
+  String sexo;
+
+  Data({required this.nombre, required this.sexo});
+}
